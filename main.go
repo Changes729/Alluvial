170816fc351c9095af8c7c8a67cb8a1a8e6a8328
Changes729 <changes0729@163.com>
2023-04-29 20:01:38 +0800

    [BUG #2] 鉴权支持

--- main.go
+++ main.go
@@ -6,9 +6,44 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/sessions"
 )
 
+var (
+	// key must be 16, 24 or 32 bytes long (AES-128, AES-192 or AES-256)
+	key   = []byte("super-secret-key")
+	store = sessions.NewCookieStore(key)
+)
+
+func login(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "cookie-name")
+
+	// Authentication goes here
+	// ...
+
+	// Set user as authenticated
+	session.Values["authenticated"] = true
+	session.Save(r, w)
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "cookie-name")
+
+	// Revoke users authentication
+	session.Values["authenticated"] = false
+	session.Save(r, w)
+}
+
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "cookie-name")
+
+	// Check if user is authenticated
+	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+
 	reader, err := r.MultipartReader()
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 	if err != nil {
@@ -27,7 +62,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 			data, _ := ioutil.ReadAll(part)
 			fmt.Printf("FormData=[%s]\n", string(data))
 		} else { // This is FileData
-			dst, _ := os.Create("./upload/" + part.FileName())
+			dst, _ := os.Create("./storage/" + part.FileName())
 			defer dst.Close()
 			io.Copy(dst, part)
 		}
@@ -36,6 +71,9 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/upload/", uploadHandler)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
+
 	fs := http.FileServer(http.Dir("./storage/"))
 	http.Handle("/blobs/", http.StripPrefix("/blobs/", fs))
 
