f4b1cd5df4611c5cac7a06b9e1ca67d73039a71a
步丶丶 <changes0729@gmail.com>
2023-05-06 14:24:52 +0800

    Ver 0.1.0 (#14)
    * [BUG] 修复上传路径错误的问题
    
    * [INTERNAL] 添加 markdown 内容访问支持，添加 网页服务器处理支持
    
    * [INTERNAL] 解决Web访问错误的问题， 更新代码实现方式
    
    * [INTERNAL] 去除跨域访问
    
    ---------
    
    Co-authored-by: Changes729 <changes0729@163.com>

--- main.go
+++ main.go
@@ -4,44 +4,63 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
 )
 
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	reader, err := r.MultipartReader()
-	w.Header().Add("Access-Control-Allow-Origin", "*")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+func AlluvialServer(r *mux.Router, prefix string, root_path string) {
+	fs := http.FileServer(http.Dir(root_path))
+	router := r.PathPrefix(prefix).Subrouter()
 
-	for {
-		part, err := reader.NextPart()
-		if err == io.EOF {
-			break
+	router.PathPrefix("/").Handler(http.StripPrefix(prefix, fs)).Methods("GET")
+	router.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		reader, err := r.MultipartReader()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 
-		fmt.Printf("FileName=[%s], FormName=[%s]\n", part.FileName(), part.FormName())
-		if part.FileName() == "" { // this is FormData``
-			data, _ := ioutil.ReadAll(part)
-			fmt.Printf("FormData=[%s]\n", string(data))
-		} else { // This is FileData
-			dst, _ := os.Create("./storage/" + part.FileName())
-			defer dst.Close()
-			io.Copy(dst, part)
+		for {
+			part, err := reader.NextPart()
+			if err == io.EOF {
+				break
+			}
+
+			fmt.Printf("FileName=[%s], FormName=[%s]\n", part.FileName(), part.FormName())
+			if part.FileName() == "" { // this is FormData``
+				data, _ := ioutil.ReadAll(part)
+				fmt.Printf("FormData=[%s]\n", string(data))
+			} else { // This is FileData
+				dst, _ := os.Create(root_path + part.FileName())
+				defer dst.Close()
+				io.Copy(dst, part)
+			}
 		}
-	}
+	}).Methods("POST")
 }
 
 func main() {
-	http.HandleFunc("/upload/", uploadHandler)
-
-	fs := http.FileServer(http.Dir("./storage/"))
-	http.Handle("/blobs/", http.StripPrefix("/blobs/", fs))
+	r := mux.NewRouter()
+	AlluvialServer(r, "/blobs", "./storage/")
+	AlluvialServer(r, "/markdowns", "./markdown/")
 
 	web := http.FileServer(http.Dir("./web/"))
-	http.Handle("/", http.StripPrefix("/", web))
 
-	http.ListenAndServe(":8080", nil)
+	r.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		fpath, _ := os.Getwd()
+		fpath += ("/web/" + r.URL.Path)
+		if _, err := os.Stat(fpath); err == nil {
+			web.ServeHTTP(w, r)
+		} else {
+			log.Print(err)
+			http.ServeFile(w, r, "./web/index.html")
+		}
+
+	})
+
+	http.ListenAndServe(":8080", r)
 }
