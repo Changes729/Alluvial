a5396e8c953118e8aff2a1e658fa9afca8ae982b
Changes729 <changes0729@163.com>
2023-05-06 14:13:48 +0800

    [INTERNAL] 解决Web访问错误的问题， 更新代码实现方式

--- main.go
+++ main.go
@@ -4,57 +4,63 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
-	"regexp"
 
 	"github.com/gorilla/mux"
 )
 
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	reader, err := r.MultipartReader()
-	w.Header().Add("Access-Control-Allow-Origin", "*")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+func AlluvialServer(r *mux.Router, prefix string, root_path string) {
+	fs := http.FileServer(http.Dir(root_path))
+	router := r.PathPrefix(prefix).Subrouter()
 
-	for {
-		part, err := reader.NextPart()
-		if err == io.EOF {
-			break
+	router.PathPrefix("/").Handler(http.StripPrefix(prefix, fs)).Methods("GET")
+	router.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		reader, err := r.MultipartReader()
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 
-		fmt.Printf("FileName=[%s], FormName=[%s]\n", part.FileName(), part.FormName())
-		if part.FileName() == "" { // this is FormData``
-			data, _ := ioutil.ReadAll(part)
-			fmt.Printf("FormData=[%s]\n", string(data))
-		} else { // This is FileData
-			dst, _ := os.Create("./storage/" + part.FileName())
-			defer dst.Close()
-			io.Copy(dst, part)
+		for {
+			part, err := reader.NextPart()
+			if err == io.EOF {
+				break
+			}
+
+			fmt.Printf("FileName=[%s], FormName=[%s]\n", part.FileName(), part.FormName())
+			if part.FileName() == "" { // this is FormData``
+				data, _ := ioutil.ReadAll(part)
+				fmt.Printf("FormData=[%s]\n", string(data))
+			} else { // This is FileData
+				dst, _ := os.Create(root_path + part.FileName())
+				defer dst.Close()
+				io.Copy(dst, part)
+			}
 		}
-	}
+	}).Methods("POST")
 }
 
 func main() {
 	r := mux.NewRouter()
+	AlluvialServer(r, "/blobs", "./storage/")
+	AlluvialServer(r, "/markdowns", "./markdown/")
 
-	fs := http.FileServer(http.Dir("./storage/"))
-	markdown := http.FileServer(http.Dir("./markdown/"))
 	web := http.FileServer(http.Dir("./web/"))
 
-	fileMatcher := regexp.MustCompile(`\.[a-zA-Z]*$`)
-
-	r.HandleFunc("/upload/", uploadHandler).Methods("POST")
-	r.PathPrefix("/blobs/").Handler(http.StripPrefix("/blobs/", fs)).Methods("GET")
-	r.PathPrefix("/markdowns/").Handler(http.StripPrefix("/markdowns/", markdown)).Methods("GET")
 	r.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !fileMatcher.MatchString(r.URL.Path) {
-			http.ServeFile(w, r, "./web/index.html")
-		} else {
+
+		fpath, _ := os.Getwd()
+		fpath += ("/web/" + r.URL.Path)
+		if _, err := os.Stat(fpath); err == nil {
 			web.ServeHTTP(w, r)
+		} else {
+			log.Print(err)
+			http.ServeFile(w, r, "./web/index.html")
 		}
+
 	})
 
 	http.ListenAndServe(":8080", r)
